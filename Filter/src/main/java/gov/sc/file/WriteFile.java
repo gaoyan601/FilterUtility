9bca26b2c71279e63c8cc7f91321a3f59209f5f0
gaoyan601 <347259020@qq.com>
2016-06-04 17:28:00 +0800

    delete class File from package gov.sc.file delete class systemRead from package gov.sc.utils delete method dicSort from class WriteFile change element type of parameter of method write in class WriteFile to List<String[]>

--- Filter/src/main/java/gov/sc/file/WriteFile.java
+++ Filter/src/main/java/gov/sc/file/WriteFile.java
@@ -2,9 +2,6 @@ package gov.sc.file;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -20,7 +17,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author Kevin
  *
  */
-public class WriteFile extends File {
+public class WriteFile{
 
 	private String file;
 
@@ -55,7 +52,7 @@ public class WriteFile extends File {
 	 * @throws IOException
 	 */
 	@SuppressWarnings("resource")
-	public void write(List<List<String>> list) throws IOException {
+	public void write(List<String[]> list) throws IOException {
 		Workbook workbook;
 		if (file.endsWith(".xls")) {
 			workbook = new HSSFWorkbook();
@@ -64,40 +61,17 @@ public class WriteFile extends File {
 		}
 		Sheet sheet = workbook.createSheet("sheet1");
 		
-		//为了不让原list改变。
-		List<List<String>> copyList = new ArrayList<List<String>>(list);	
-		dictSort(copyList);
-		
-		for (int i = 0; i < copyList.size(); i++) {
-			List<String> rowList = copyList.get(i);
+		for (int i = 0; i < list.size(); i++) {
+			String[] rowList = list.get(i);
 			Row row = sheet.createRow(i);
-			for (int j = 0; j < rowList.size(); j++) {
+			for (int j = 0; j < rowList.length; j++) {
 				Cell cell = row.createCell(j);
-				cell.setCellValue(rowList.get(j));
+				cell.setCellValue(rowList[j]);
 			}
 		}
 		FileOutputStream fout = new FileOutputStream(file);
 		workbook.write(fout);
 		fout.flush();
 		fout.close();
-	}
-	
-	/**
-	 * 对list排序的字典排序算法
-	 * 
-	 * @param list
-	 * @throws IOException
-	 */
-	@SuppressWarnings({ "unused", "unchecked" })
-	private void dictSort(List<List<String>> list) throws IOException
-	{
-		Collections.sort(list, new Comparator<List<String>>()
-		{
-			public int compare(List<String> o1, List<String> o2)
-			{
-				// TODO 自动生成的方法存根
-				return o1.get(0).compareTo(o2.get(0));
-			}
-		});
 	}
 }
