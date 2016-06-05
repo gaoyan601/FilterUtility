ed50e8995861d833d4b147afe881df0a8fb9dd8f
zzz01 <zhangzhizhao3@qq.com>
2016-06-05 08:50:34 +0800

    主窗体 - 选择按钮事件处理

---
+++ Filter/src/main/java/gov/sc/form/listener/ScanButHandler.java
@@ -0,0 +1,53 @@
+package gov.sc.form.listener;
+
+import gov.sc.form.Gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
+
+
+public class ScanButHandler implements ActionListener {
+
+	private Gui gui;
+
+	public ScanButHandler(Gui gui) {
+		this.gui = gui;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JTextField srcPthTxtFiled = new JTextField();
+		srcPthTxtFiled = gui.srcPthTxtFiled;
+		JProgressBar proBar = new JProgressBar();
+		proBar = gui.progressbar;
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		chooser.setFileFilter(new FileFilter() {
+			public boolean accept(File file) { // 设定可用的文件的后缀名 || f.isDirectory()
+				if (file.getName().endsWith(".xls")
+						|| file.getName().endsWith(".xlsx")
+						|| file.isDirectory()) {
+					return true;
+				}
+				return false;
+			}
+
+			public String getDescription() {
+				return "Excel文件(*.xls/xlsx)";
+			}
+		});
+		if (chooser.showDialog(null, "选择") == JFileChooser.CANCEL_OPTION) {
+			return;
+		}
+		srcPthTxtFiled.setText(chooser.getSelectedFile().toString());
+		proBar.setString("请选择正确的目标列和时间列");
+
+		
+
+	}
+}
