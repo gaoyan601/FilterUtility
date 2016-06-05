5cbf6e13eb7c93e909c825bf934a88b72c7b679a
zzz01 <zhangzhizhao3@qq.com>
2016-06-05 08:45:37 +0800

    文件

---
+++ Filter/src/main/java/gov/sc/form/FileInfo.java
@@ -0,0 +1,31 @@
+package gov.sc.form;
+
+public class FileInfo {
+	protected String reFile;
+	protected String tarSave;
+	protected String tarColumn;
+	public String getReFile() {
+		return reFile;
+	}
+
+	public void setReFile(String reFile) {
+		this.reFile = reFile;
+	}
+
+	public String getTarSave() {
+		return tarSave;
+	}
+
+	public void setTarSave(String tarSave) {
+		this.tarSave = tarSave;
+	}
+
+	public String getTarColumn() {
+		return tarColumn;
+	}
+
+	public void setTarColumn(String tarColumn) {
+		this.tarColumn = tarColumn;
+	}
+
+}
