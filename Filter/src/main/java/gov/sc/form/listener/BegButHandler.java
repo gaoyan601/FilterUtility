49c10827eb79b19b3657515fc829ae6540547680
gaoyan601 <347259020@qq.com>
2016-06-06 16:00:00 +0800

    修复了停用词配置窗体关闭点击否无法关闭的bug

--- Filter/src/main/java/gov/sc/form/listener/BegButHandler.java
+++ Filter/src/main/java/gov/sc/form/listener/BegButHandler.java
@@ -48,6 +48,9 @@ public class BegButHandler implements ActionListener {
 		private List<String[]> cells;
 		private int tarline;
 		private int timeline;
+		private String[] searchTime;
+		private int timeIndex = 0;
+		private int tarIndex = 0;
 
 		public HandleThread(MainForm form) {
 			this.proBar = form.progressbar;
@@ -93,10 +96,20 @@ public class BegButHandler implements ActionListener {
 				logger.info("read Excel file error---->" + e.toString());
 				return;
 			}
+			searchTime = cells.get(0);
+			for(int i =0;i<searchTime.length;i++){
+				if(searchTime[i].matches(".*时间.*")||searchTime[i].matches(".*日期.*")){
+					timeIndex = i;
+				}
+				if(searchTime[i].matches("标题")){
+					tarIndex = i;
+				}
+			}
+			
 			int value = cells.size();
 			proBar.setString("文件解析中...");
-			tarline = selectTarCol.getSelectedIndex();
-			timeline = selectTarTim.getSelectedIndex();
+			tarline = selectTarCol.getSelectedIndex()+tarIndex;
+			timeline = selectTarTim.getSelectedIndex()+timeIndex;
 			Cluster cluster = new Cluster(cells, tarline, timeline);
 			List<String[]> reList = cluster.getResult_all();
 			proBar.setValue(value * 2);
